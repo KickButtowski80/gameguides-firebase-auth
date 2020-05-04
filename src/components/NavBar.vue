4227f4b967d2d089e49a617754d4e9557b0555e8
kickbuttowski80 <pazpaz25@gmail.com>
2020-05-03 21:09:20 -0700

    setup themes colors

---
+++ src/components/NavBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+      </div>
+      <v-spacer></v-spacer>
+      <div v-for="item in items" :key="item.text" class="primary--text">
+        <v-btn v-text="item.text"></v-btn>
+      </div>
+    </v-app-bar>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { text: "Account", link: "#" },
+        { text: "Logout", link: "#" },
+        { text: "Create Guide", link: "#" },
+        { text: "Login", link: "#" },
+        { text: "Signin", link: "#" }
+      ]
+    };
+  }
+};
+</script>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,21 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme:{
+        themes:{
+            light:{
+                happy: "#FFDAB9",
+                primary: '#333333',
+                secondary: '#2B2B2B',
+                white: '#ffffff',
+                orange: '#FC6D2B',
+                darkgrey: '#707070',
+                complete: '#2BFC83',
+                inprogress: '#FF8936',
+                incomplete: '#FF1212',
+                Unstarted: '#FF1212',
+                lightgrey: '#cacaca'
+            }
+        }
+    }
 });
