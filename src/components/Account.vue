a6edf4579820a4b964b8fa76605a523d98e2c4a0
kickbuttowski80 <pazpaz25@gmail.com>
2020-05-04 18:48:23 -0700

    create dialog that shows the account information for the current user

---
+++ src/components/Account.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-row justify="center">
+    <v-dialog v-model="dialog" persistent max-width="600px">
+      <template v-slot:activator="{ on }">
+        <v-btn outlined class="primary z-depth-0 darken" v-on="on">Account</v-btn>
+      </template>
+      <v-form ref="form"> 
+        <v-card>
+          <v-card-title>
+            <span class="headline">Account Details</span>
+          </v-card-title>
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-col cols="12">put account details here</v-col>
+              </v-row>
+            </v-container>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" text @click="dialog = false">Cancel</v-btn>
+            <v-btn color="blue darken-1" text @click="dialog = false">Ok</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-form>
+    </v-dialog>
+  </v-row>
+</template>
+<script>
+export default {
+  name: "Account",
+  data() {
+    return {
+      dialog: false
+    };
+  }
+};
+</script>
+<style lang="css" scoped >
+</style>
