df3a44fca561de0599038d02144c9afbb32c991b
kickbuttowski80 <pazpaz25@gmail.com>
2020-05-05 18:14:09 -0700

    implemented login process for an user

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -13,10 +13,10 @@
             <v-container>
               <v-row>
                 <v-col cols="12">
-                  <v-text-field label="Email*" required></v-text-field>
+                  <v-text-field v-model="email" label="Email*" required></v-text-field>
                 </v-col>
                 <v-col cols="12">
-                  <v-text-field label="Password*" type="password" required></v-text-field>
+                  <v-text-field v-model="password" label="Password*" type="password" required></v-text-field>
                 </v-col>
               </v-row>
             </v-container>
@@ -24,19 +24,34 @@
           <v-card-actions>
             <v-spacer></v-spacer>
             <v-btn color="blue darken-1" text @click="dialog = false">Cancel</v-btn>
-            <v-btn color="blue darken-1" text @click="dialog = false">Log In</v-btn>
+            <v-btn color="blue darken-1" text @click="loginUser">Log In</v-btn>
           </v-card-actions>
         </v-card>
       </v-form>
     </v-dialog>
   </v-row>
 </template>
-
 <script>
 export default {
   name: "Login",
-  data: () => ({
-    dialog: false
-  })
+  data() {
+    return {
+      email: null,
+      password: null,
+      dialog: false,
+    };
+  },
+  methods: {
+    loginUser() {
+      if (this.$refs.form.validate()) {
+        this.$store.dispatch("loginUser", {
+          email: this.email,
+          password: this.password
+        });
+        this.$refs.form.reset();
+        this.dialog = false
+      }
+    }
+  }
 };
 </script>
--- src/store/store.js
+++ src/store/store.js
@@ -19,23 +19,41 @@ export const store = new Vuex.Store({
   },
   actions: {
     async signupUser({ commit }, payload) {
-      const user_credtential = await auth.createUserWithEmailAndPassword(payload.email, payload.password)
-      console.log(user_credtential)
-      if (!user_credtential) {
-        console.log(user_credtential)
+      try {
+        // const user_credtential =
+        await auth.createUserWithEmailAndPassword(payload.email, payload.password)
+        // console.log(user_credtential)
+        commit("setUser", payload)
+      } catch (error) {
+        console.log(error.message)
       }
-      commit("setUser", payload)
     },
 
     async logoutUser({ commit }) {
-      const logout_the_user = await auth.signOut()
-      console.log(logout_the_user)
-      if (logout_the_user === undefined) {
+      try {
+        // const logout_the_user =
+        await auth.signOut()
         commit('setUser', null)
         console.log("successful logout")
-      } else {
-        console.log("something went wrong when u tried to logout")
+      } catch (error) {
+        console.log(error.message)
+      }
+    },
+
+    async loginUser({ commit }, payload) {
+      //to be sure there is anotehr user who is logged in
+      commit('setUser', null)
+      try {
+        // const loggedin_user = // shows user credentials 
+        await auth.signInWithEmailAndPassword(payload.email, payload.password)
+        // console.log(loggedin_user.user)
+        commit('setUser', payload)
+      } catch (error) {
+        alert(error.message);
       }
     }
+
+
+
   }
 })
