cf4c8dac6cb343c9ff5bc4a1b7499a280a97ce41
kickbuttowski80 <pazpaz25@gmail.com>
2020-05-06 22:00:50 -0700

    added alert that informed an user for empty guides list when there is no guide inside the list and the alert goes away when the user adds a guide to the list

--- src/components/GuideLists.vue
+++ src/components/GuideLists.vue
@@ -1,14 +1,23 @@
 <template>
   <v-container>
     <v-row>
-      <v-col>{{currentUserInfo}}</v-col>
+      <v-col>{{ currentUserInfo }}</v-col>
     </v-row>
     <v-row>
       <v-col>
-        <v-expansion-panels>
+        <span v-if="emptyGuides">
+          <v-alert type="info" icon="mdi-emoticon-sad">
+            Guides List is empty
+          </v-alert>
+        </span>
+        <v-expansion-panels v-else>
           <v-expansion-panel v-for="item in guides" :key="item.title">
-            <v-expansion-panel-header>{{item.title}}</v-expansion-panel-header>
-            <v-expansion-panel-content class="lightgrey">{{item.body}}</v-expansion-panel-content>
+            <v-expansion-panel-header>{{
+              item.title
+            }}</v-expansion-panel-header>
+            <v-expansion-panel-content class="lightgrey">{{
+              item.body
+            }}</v-expansion-panel-content>
           </v-expansion-panel>
         </v-expansion-panels>
       </v-col>
@@ -25,17 +34,20 @@ export default {
         { title: "Guide title1", body: "Lorem ipsum dolor sit amet" },
         { title: "Guide title2", body: "Lorem ipsum dolor sit amet" },
         { title: "Guide title3", body: "Lorem ipsum dolor sit amet" },
-        { title: "Guide title4", body: "Lorem ipsum dolor sit amet" }
-      ]
+        { title: "Guide title4", body: "Lorem ipsum dolor sit amet" },
+      ],
     };
   },
   computed: {
     currentUserInfo() {
       return this.$store.getters.user;
     },
-    guides(){
-      return this.$store.getters.guides
+    guides() {
+      return this.$store.getters.guides;
+    },
+    emptyGuides(){
+      return this.$store.getters.guides.length === 0 
     }
-  }
+  },
 };
 </script>
